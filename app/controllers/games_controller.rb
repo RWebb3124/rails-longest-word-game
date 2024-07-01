72e64fa18b36a25d147b6379933d8c993201ba7a
Ryan Webb <rwebb3124@gmail.com>
2024-07-01 14:22:17 +0100

    challenge completed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,33 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
+
   def new
-    @numbers = Array.new(10) { ('A'..'Z').to_a.sample }
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def included?(answer, grid)
+    answer.downcase.chars.all? { |letter| answer.count(letter) <= grid.downcase.count(letter) }
+  end
+
+  def valid_word?(answer)
+    url = "https://dictionary.lewagon.com/#{answer}"
+    response = URI.open(url).read
+    page = JSON.parse(response)
+    page['found']
   end
 
   def score
+    @grid = params[:grid]
+    @answer = params[:answer]
+    if included?(@answer, @grid)
+      if valid_word?(@answer)
+        @response = "<b>Congratulations!</b> #{@answer} is a correct and real english word, and is <b>#{@answer.length}</b> letters long".html_safe
+      else
+        @response = "Sorry, but <b>#{@answer}</b> isn't a real english word".html_safe
+      end
+    else
+      @response = "Sorry, but <b>#{@answer}</b> can't be built out of <b>#{@grid}</b>".html_safe
+    end
   end
 end
